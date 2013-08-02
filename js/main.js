91464f26749e8f9173bd42bc45e7795bcc52e932
Garrett Dawson <killtheliterate@gmail.com>
2013-08-02 15:25:40 -0600

    Leave me alone ok broohafux

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ var path = new Path();
 var textz = '';
 var rotate = 5;
 var freakout = false;
+var hi = 'I hate you';
 
 // Give the stroke a color
 path.strokeColor = 'black';
@@ -92,11 +93,14 @@ function onFrame(event) {
 
 $('#chule-dood').change(function(){
   textz = $(this).val();
+  hi = colourNameToHex(textz);
+
+  console.log(hi);
+
   $(this).val('');
 });
 
-
-function colorNameToHex(colour)
+function colourNameToHex(colour)
 {
     var colours = {"aliceblue":"#f0f8ff","antiquewhite":"#faebd7","aqua":"#00ffff","aquamarine":"#7fffd4","azure":"#f0ffff",
     "beige":"#f5f5dc","bisque":"#ffe4c4","black":"#000000","blanchedalmond":"#ffebcd","blue":"#0000ff","blueviolet":"#8a2be2","brown":"#a52a2a","burlywood":"#deb887",
@@ -128,16 +132,3 @@ function colorNameToHex(colour)
 
     return false;
 }
-
-$('#chule-dood').change(function(){
-  var origCol = $(this).val();
-  var origHex = colorNameToHex(origCol);
-
-  // var newNew = origCol.split('').reverse().join('');
-
-  console.log(origCol);
-  console.log(origHex);
-  // console.log(newNew);
-
-  // $('body').css('background', origCol);
-});
